419948db1bd50963d29037f86f3c0b5eece367de
matthewlekman <matthew@lekman.com>
2025-08-05 22:08:52 +0100

    Created Login form UI

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Authentication Screen
+
 Simple React based project, which handles the authentication of a user's username and password.
--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import companyLogo from './assets/logo.svg';
+import './styles/LogoStyles.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className="glass-box">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={companyLogo} className="logo center-wrapper" alt="Company Logo" />
+        <h1>Signal</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="center">
+        <label>username: </label>
+        <input></input>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="center">
+        <label>password: </label>
+        <input></input>
+      </div>
+      <div className="center">
+        <button>submit</button>
+      </div>
+      <div className="center">
+        <hr className="divider"/>
+      </div>
+      <div className="forgot">
+        <button>forgot password?</button>
+      </div>
+    </div>
     </>
   )
 }
